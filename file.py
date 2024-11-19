ec6c044d349a8802d845ae3f627a77b6521fc3df
chanellesf <chfriend@calpoly.edu>
2024-11-18 17:13:19 -0800

    completed lab 8

--- file.py
+++ file.py
@@ -1,7 +1,9 @@
 import sys
 from typing import Optional
 
-
+# Returns the float sum of two elements in a given list, converts the elements to float before computing
+# INPUT: list with two elements of str
+# OUTPUT: float sum
 def str_to_float_sum(text : list[str]) -> Optional[float]:
     try:
         return float(text[0]) + float(text[1])
@@ -13,12 +15,13 @@ def main():
     file_name = sys.argv[1]
     infile = open(file_name)
     count = 0
-    for line in infile:
-        x = line.split()
-        count += 1
-        print("[LINE: ", count, "]: SUM =", str_to_float_sum(x))
+    with open(file_name, "r") as file:
+        for line in infile:
+            x = line.split()
+            count += 1
+            print("[LINE: ", count, "]: SUM =", str_to_float_sum(x))
 
 try:
     main()
-except:
+except FileNotFoundError:
     print("Error: file not found")
